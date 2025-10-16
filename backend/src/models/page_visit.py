4d08e71e323b1aaad4f4d6dc2e8693265c9dfec5
Caleb Olusola <calebseyi123@gmail.com>
2025-10-16 19:44:51 +0100

    feat: page visit model

---
+++ backend/src/models/page_visit.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+
+import pytz
+from sqlalchemy import Column, Integer, DateTime, Text
+
+from src.core.db.database import Base
+
+
+def get_utc_now():
+    return datetime.now(pytz.UTC)
+
+
+class PageVisit(Base):
+    __tablename__ = "visits"
+
+    id = Column(Integer, primary_key=True, index=True)
+    datetime_visited = Column(DateTime(timezone=True), default=get_utc_now, nullable=False)
+    url = Column(Text, nullable=False, index=True)
+    link_count = Column(Integer, nullable=False)
+    word_count = Column(Integer, nullable=False)
+    image_count = Column(Integer, nullable=False)
+
+    def to_dict(self, exclude=None):
+        if exclude is None:
+            exclude = []
+        return {
+            column.name: getattr(self, column.name)
+            for column in self.__table__.columns.values()
+            if column.name not in exclude
+        }
